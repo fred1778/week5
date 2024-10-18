32182cd536ef4ab224b5489f6cd31a89f6441c52
May Daoud <zy21368@it106296.wks.bris.ac.uk>
2024-10-18 10:31:01 +0100

    suck it:(

--- goodmorning.c
+++ goodmorning.c
@@ -3,14 +3,6 @@
 
 bool mycodeisthebest(void);
 
-int main (void){
-
-  printf("Welcome to the repo!"\n);
-  bool the_truth = mycodeisthebest();
-
-
-return 0;
-}
-bool mycodeisthebest(void){
-  return true;
-}
+NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+NO
+NO:(
