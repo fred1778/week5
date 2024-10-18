11045960549e68c677b5e4909707eb8dd70b5cb2
Fred Clamp-Gray <ey24984@it106297.wks.bris.ac.uk>
2024-10-18 10:17:24 +0100

    updated goodmorning

--- goodmorning.c
+++ goodmorning.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+
+bool mycodeisthebest(void);
+
 int main (void){
 
   printf("Welcome to the repo!"\n);
+  bool the_truth = mycodeisthebest();
+
 
 return 0;
+}
+bool mycodeisthebest(void){
+  return true;
 }
